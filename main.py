6e3a68200098a32ce08b67ca5b29e0368579e399
Dushyant Vishwakarma <dushyantss8@gmail.com>
2025-07-22 22:15:06 +0530

    Add update post API

--- main.py
+++ main.py
@@ -102,3 +102,18 @@ def delete_post(id: int):
     my_posts.pop(index)
     # return { "message": "Post deleted successfully!" }  # Id deleting then dont return json like this
     return Response(status_code=status.HTTP_204_NO_CONTENT) # Instead return a response like this
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    index = find_post_index(id)
+    
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found.")
+    
+    post_dict = post.model_dump()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {
+        "message": "Post updated successfully!",
+        "post": post_dict
+    }
